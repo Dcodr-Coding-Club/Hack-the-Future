efe028c79e4d99f74a696b614cd4c4a973cfb198
Sarthak-Daga <sarthakdaga21@gmail.com>
2025-03-29 20:33:05 +0530

    ForTesting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import { Route, BrowserRouter as Router, Routes, useLocation } from "react-router-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import Navigation from "./components/Navigation";
 import { Navbar } from "./components";
-
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 
-
-import { About, Contact, Home, Projects, Profile , Aboutus } from "./pages"; // Import Profile Page
-
+import { About, Contact, Home, Projects, Profile, Aboutus } from "./pages"; // Import Profile Page
 
 const App = () => {
   return (
@@ -22,11 +24,10 @@ const MainContent = () => {
 
   return (
     <main className="bg-slate-300/20 relative">
-      {location.pathname === "/" && <Navigation />} 
+      {location.pathname === "/" && <Navigation />}
 
       <Navbar />
       <Routes>
-      
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/" element={<Home />} />
@@ -34,7 +35,8 @@ const MainContent = () => {
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/Aboutus" element={<Aboutus />} /> {/* Add Profile Route */}
+        <Route path="/Aboutus" element={<Aboutus />} />{" "}
+        {/* Add Profile Route */}
       </Routes>
     </main>
   );
