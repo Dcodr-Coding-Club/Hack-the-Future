a8c3152c9e952b691ed9504b36cc415fd267ec85
Ayush16Mishra <Ayushpc025@gmail.com>
2025-03-30 23:52:09 +0530

    Final Commit

--- frontend/src/components/Navigation/data.js
+++ frontend/src/components/Navigation/data.js
@@ -8,7 +8,7 @@ import twitter from './Twitter.jpg';
 import resume from './Resume.jpg';
 
 export const BtnList = [
-  { label: "Home", link: "/progress", image: home, newTab: false },
+  { label: "Home", link: "/", image: home, newTab: false },
   { label: "Courses", link: "/about", image: about, newTab: false },
   { label: "Classes", link: "/projects", image: projects, newTab: false },
   { label: "Contact", link: "/contact", image: contact, newTab: false },
