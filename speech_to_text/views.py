f8b7b8838292fb5257a3bfdd51a428f7985cb71c
aeshaladani <ashaladani11@gmail.com>
2025-03-30 23:39:28 +0530

    lastpush

--- speech_to_text/views.py
+++ speech_to_text/views.py
@@ -3,10 +3,10 @@ from django.views.decorators.csrf import csrf_exempt
 import speech_recognition as sr
 from django.http import JsonResponse
 from django.shortcuts import render
- 
+
 def speech_to_text_home(request):
     return render(request, "index.html")
- 
+
 @csrf_exempt  # Add this to disable CSRF for this function
 def record_and_transcribe(request):
     if request.method == "POST":
@@ -15,7 +15,7 @@ def record_and_transcribe(request):
             print("Say something... (Recording)")
             recognizer.adjust_for_ambient_noise(source)  # Reduce background noise
             audio = recognizer.listen(source)  # Capture audio
- 
+
         try:
             print("Converting speech to text...")
             text = recognizer.recognize_google(audio)  # Convert speech to text
@@ -25,5 +25,5 @@ def record_and_transcribe(request):
             return JsonResponse({"error": "Could not understand the audio"}, status=400)
         except sr.RequestError:
             return JsonResponse({"error": "Error with the speech recognition service"}, status=500)
-     
+    
     return JsonResponse({"message": "Send a POST request to transcribe speech"}, status=405)
